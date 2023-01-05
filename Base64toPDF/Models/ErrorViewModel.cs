c83090b1b860d09c95dfaf91eb348245bbc7460a
JoseMgz <joseutp14@gmai.com>
2023-01-05 16:03:04 -0500

    Agregar archivos de proyecto.

---
+++ Base64toPDF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Base64toPDF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Base64toPDF/Models/ModelBase64.cs
@@ -0,0 +1,7 @@
+﻿namespace Base64toPDF.Models
+{
+    public class ModelBase64
+    {
+        public string Base64 { get; set; }
+    }
+}
